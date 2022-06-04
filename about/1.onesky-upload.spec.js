e413c9f30eb4ab49cce130c66846023a9a3ec8e9
ericminio <eric.mignot@gmail.com>
2022-06-03 20:51:50 -0700

    extract noise

---
+++ about/1.onesky-upload.spec.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+const { upload } = require('../lib/onesky');
+const { Server } = require('../support/server');
+const extractPayload = require('../lib/extract-payload');
+const { parse } = require('../lib/form-data-protocol');
+const port = 5001;
+
+describe('OneSky upload', () => {
+    
+    let server;
+    let received;
+
+    beforeEach((done) => {
+        server = new Server(port, async (request, response) => {
+            let payload = await extractPayload(request);
+            received = parse({ payload });
+            response.writeHead(201, { 'content-Type': 'text/plain' });
+            response.end('Created');
+        })
+        server.start(done);
+    });
+    afterEach((done) => {
+        server.stop(done);    
+    });
+
+    it('includes the required fields', async () => {
+        await upload({
+            fileName: 'hello.yml',
+            fileFormat: 'YAML',
+            content: 'hello OneSky'
+        });
+        expect(received).to.deep.equal({
+            form: [
+                { name: 'file', fileName: 'hello.yml', value: 'hello OneSky' },
+                { name: 'file_format', value: 'YAML' },
+            ]
+        });
+    });
+});
+
